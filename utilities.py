3591636d8b345a8fc476454c034883850be4d290
AlexAndorra <andorra.alexandre@gmail.com>
2021-01-23 21:55:19 +0100

    Add NCDF files for Bokeh popularity plots

--- utilities.py
+++ utilities.py
@@ -23,29 +23,27 @@ from scipy.special import expit as logistic
 
 
 # @st.cache
-def extract_constant_data(repo_path: str):
+def extract_constant_data():
     """
     Extract all the constant data needed to make the Bokeh plot and Streamlit app.
     """
-    _complete_data = pd.read_csv(
-        "https://raw.githubusercontent.com/AlexAndorra/pollsposition_models/master/popularity/plot_data/complete_popularity_data.csv",
+    BASE_URL = "https://raw.githubusercontent.com/AlexAndorra/pollsposition_models/master/popularity/plot_data"
+    _complete_data = pd.read_csv(f"{BASE_URL}/complete_popularity_data.csv",
         index_col=0,
         parse_dates=True,
     )
-    _raw_polls = pd.read_csv(
-        "https://raw.githubusercontent.com/AlexAndorra/pollsposition_models/master/popularity/plot_data/raw_polls.csv",
+    _raw_polls = pd.read_csv(f"{BASE_URL}/raw_polls.csv",
         index_col=0,
         parse_dates=True,
     )
-    _prediction_coords = pd.read_csv(
-        "https://raw.githubusercontent.com/AlexAndorra/pollsposition_models/master/popularity/plot_data/prediction_coords.csv",
+    _prediction_coords = pd.read_csv(f"{BASE_URL}/prediction_coords.csv",
         index_col=0,
         parse_dates=["timesteps"],
     )
-    _trace_econ = az.from_netcdf(f"{repo_path}/trace_raw_econ.nc")
-    _pp_prop = xr.open_dataset(f"{repo_path}/plot_data/post_pred_approval.nc")
-    _pp_prop_5 = xr.open_dataset(f"{repo_path}/plot_data/post_pred_approval_5.nc")
-    _pp_prop_10 = xr.open_dataset(f"{repo_path}/plot_data/post_pred_approval_10.nc")
+    _trace_predictions = az.from_netcdf("plot_data/trace_predictions.nc")
+    _pp_prop = xr.open_dataset("plot_data/post_pred_approval.nc")
+    _pp_prop_5 = xr.open_dataset("plot_data/post_pred_approval_5.nc")
+    _pp_prop_10 = xr.open_dataset("plot_data/post_pred_approval_10.nc")
 
     _source_annotations = ColumnDataSource(
         data=dict(
@@ -73,7 +71,7 @@ def extract_constant_data(repo_path: str):
         _complete_data,
         _raw_polls,
         _prediction_coords,
-        _trace_econ,
+        _trace_predictions,
         _pp_prop,
         _pp_prop_5,
         _pp_prop_10,
@@ -86,14 +84,12 @@ def extract_constant_data(repo_path: str):
     complete_data,
     raw_polls,
     PREDICTION_COORDS,
-    trace_econ,
+    trace_predictions,
     pp_prop,
     pp_prop_5,
     pp_prop_10,
     SOURCE_ANNOTATIONS,
-) = extract_constant_data(
-    repo_path="/Users/alex_andorra/repos/pollsposition_models/popularity"
-)
+) = extract_constant_data()
 
 
 def generate_app_input() -> Tuple[str, gridplot, pd.DataFrame]:
@@ -113,7 +109,7 @@ def generate_app_input() -> Tuple[str, gridplot, pd.DataFrame]:
         pp_prop_10["post_pred_approval_10"],
     ]
     for post_pred_approval in post_pred_approval_list:
-        source_df = generate_bokeh_data_source(trace_econ, post_pred_approval)
+        source_df = generate_bokeh_data_source(trace_predictions, post_pred_approval)
         random_draws = samples_subset(source_df)
 
         source_df_list.append(source_df)
@@ -142,7 +138,7 @@ def generate_bokeh_data_source(
     """
     Turn posterior inference data into a dataframe for Bokeh.
 
-    :param trace: inference data from the model.
+    :param trace: only the ``predictions`` group of the full inference data from the model.
     :param post_pred_samples: posterior predictive approval of president, under given unemployment.
 
     :return: posterior inference data as a dataframe.
@@ -154,12 +150,12 @@ def generate_bokeh_data_source(
     )
     source_df.columns = source_df.columns.astype(str)
 
-    source_df["baseline"] = logistic(trace.predictions["baseline"]).mean().data
+    source_df["baseline"] = logistic(trace.posterior["baseline"]).mean().data
     source_df["baseline_lower"] = (
-        logistic(az.hdi(trace.predictions)["baseline"]).sel(hdi="lower").data
+        logistic(az.hdi(trace.posterior)["baseline"]).sel(hdi="lower").data
     )
     source_df["baseline_upper"] = (
-        logistic(az.hdi(trace.predictions)["baseline"]).sel(hdi="higher").data
+        logistic(az.hdi(trace.posterior)["baseline"]).sel(hdi="higher").data
     )
 
     source_df["median_app"] = post_pred_samples.median(dim=("chain", "draw")).data
